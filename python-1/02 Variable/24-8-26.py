d44e5021e706858f7cbfc19260e4115402ff0203
shivam <shivam.kotadiya.cg@gmail.com>
2026-08-24 22:17:40 +0530

    done

---
+++ python-1/02 Variable/24-8-26.py
@@ -0,0 +1,52 @@
+# question 23
+age = 18
+height = 5.8
+name = "shivam"
+is_student = True
+result = none
+
+# question 24
+student_name = "Shivam"
+student_age = 18
+student_height = 5.8
+is_student = True
+student_result = None
+
+# question 25
+a = 50
+b = 50.0
+c = "50"
+
+
+#question 26
+a = True
+b = "True"
+
+# question 27
+a = None
+b = "None"
+
+# question 28
+value = 100
+#print(type(value))
+
+value = "Hello"
+#print(type(value))
+
+
+#question 29
+product_name = "Laptop"
+product_quantity = 5
+product_price = 55000.50
+product_available = True
+product_discount = None
+
+#question 30
+a = 10
+b = 10.0
+c = "10"
+d = True
+e = "True"
+f = None
+g = "None"
+
